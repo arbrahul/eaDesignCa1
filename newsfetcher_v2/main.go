54bd565c6045be7ffc9064d554ccc4b4eea1c567
Rahul Ayyampully <rahay@microsoft.com>
2021-05-08 13:03:04 +0100

    changes

---
+++ newsfetcher_v2/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"math/rand"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Newsfetcher V2: No news! Is good news ;-)")
+}
+
+func main() {
+
+	if rand.Intn(90) < 30 {
+		panic("flaky error occurred ")
+    } 
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8888", nil))
+}
